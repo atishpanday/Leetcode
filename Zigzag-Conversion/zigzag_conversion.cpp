2b0fb4a7932727a22609e47f68d695277bde3002
Atish Panday <atishpanday@Atishs-MacBook-Air.local>
2023-10-09 17:57:47 -0700

    added explanation

--- Zigzag-Conversion/zigzag_conversion.cpp
+++ Zigzag-Conversion/zigzag_conversion.cpp
@@ -21,3 +21,25 @@ public:
         return res;
     }
 };
+
+/*
+# j=0   # j=c   # j=2c and so on
+#     # #     #
+#   #   #   #
+# #     # #
+#       #
+
+j goes like 0, cycleLen, 2*cycleLen and so on.
+Each cycleLen is the peak of the input string in zigzag form
+The length of cycleLen is 2*(numRows - 1), we count from peak to valley 
+and back to peak - 1
+i goes from 0 to numRows - 1
+So when i = 0, we go from one column to next with j increasing by 1 cycleLen
+At each cycleLen, we check if it is not the peak or the valley, then we have to
+take care of the diagonal characters. Here we make the check:
+    (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+This checks whether our i is not at the extremes of the column, and also checks if
+our index is not out of bounds (< n)
+
+Then, j + cycleLen - i is simply the character at the ith row on the diagonal.
+*/
