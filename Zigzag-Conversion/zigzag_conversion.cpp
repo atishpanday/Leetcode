16e4bb8f8f880c9662469024a524dbf06c79b98f
Atish Panday <atishpanday@Atishs-MacBook-Air.local>
2023-10-09 17:48:44 -0700

    converts a string to a zigzag pattern, given a number of rows

---
+++ Zigzag-Conversion/zigzag_conversion.cpp
@@ -0,0 +1,23 @@
+lass Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if(numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        int cycleLen = 2 * (numRows - 1);
+        int n = s.length();
+        string res;
+
+        for(int i = 0; i < numRows; i++) {
+            for(int j = 0; i + j < n; j += cycleLen) {
+                res += s[i + j];
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    res += s[j + cycleLen - i];
+                }
+            }
+        }
+        return res;
+    }
+};
