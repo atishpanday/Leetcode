f1f4c767cb234ab577ad911a32d7fcb2f4357016
Atish Panday <atishpanday1@gmail.com>
2023-10-27 01:22:22 -0700

    added simplest solution to comments" "

--- Int-to-roman/int_to_roman.cpp
+++ Int-to-roman/int_to_roman.cpp
@@ -50,3 +50,17 @@ public:
 // 1660 = MCDLX
 
 // Basically our approach is to take each case separately: 100 to 300, 400 to 800 and 900.
+
+// BEST SOLUTION:
+
+// class Solution {
+// public:
+//     string intToRoman(int num) {
+//         string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+//         string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+//         string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+//         string ths[]={"","M","MM","MMM"};
+        
+//         return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+//     }
+// };
